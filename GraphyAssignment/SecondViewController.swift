4dca35800fc18dedac981076088b039f136d7a12
Balaji Sivaji <balaji.sivaji@cleartrip.com>
2020-05-05 00:03:46 +0530

    Initial Commit

---
+++ GraphyAssignment/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  GraphyAssignment
+//
+//  Created by Balaji S on 05/05/20.
+//  Copyright © 2020 balaji. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
